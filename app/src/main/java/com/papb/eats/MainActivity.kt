f54a05fd47d81ef013e2a6cadddc6767cd4a47c6
EzGreezy <vgriffith97@gmail.com>
2021-05-02 20:58:32 +0700

    add floating button ama jam (24h, gmt+7)

--- app/src/main/java/com/papb/eats/MainActivity.kt
+++ app/src/main/java/com/papb/eats/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.papb.eats
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.papb.eats.fragments.ReminderFragment
 import com.papb.eats.fragments.SetingsFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +31,11 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Welcome",Toast.LENGTH_SHORT).show()
     }
 
+
     private fun makeCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fl_wrapper, fragment)
             commit()
         }
+
 }
